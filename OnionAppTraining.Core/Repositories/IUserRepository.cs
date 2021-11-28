2e2eb44a253fbe4572a93bdb23e65f6f8cb2bcda
Marcin <m.pietrucha0@gmail.com>
2021-11-28 20:03:58 +0100

    user repository added

---
+++ OnionAppTraining.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using OnionAppTraining.Core.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace OnionAppTraining.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        public User GetById(Guid id);
+        public User GetByEmail(string email);
+        public IEnumerable<User> GetAll();
+        public void Add(User user);
+        public void Update(User user);
+        public void Remove(Guid id);
+    }
+}
---
+++ OnionAppTraining.Infrastructure/Repositories/InMemoryUserRepository.cs
@@ -0,0 +1,32 @@
+﻿using OnionAppTraining.Core.Domain;
+using OnionAppTraining.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnionAppTraining.Infrastructure.Repositories
+{
+    public class InMemoryUserRepository : IUserRepository
+    {
+        private static ISet<User> _users = new HashSet<User>();
+
+        public void Add(User user) => _users.Add(user);
+
+        public IEnumerable<User> GetAll() => _users;
+
+        public User GetByEmail(string email) => _users.Single(x => x.Email == email.ToLowerInvariant());
+
+        public User GetById(Guid id) => _users.Single(x => x.Id == id);
+
+        public void Remove(Guid id)
+        {
+            var user = GetById(id);
+            _users.Remove(user);
+        }
+
+        public void Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
