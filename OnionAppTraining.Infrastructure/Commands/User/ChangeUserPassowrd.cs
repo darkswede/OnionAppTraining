fe4d037248eef1b8e6b35c6dc5ddd365564aacf5
Marcin <m.pietrucha0@gmail.com>
2021-12-10 16:36:46 +0100

    changeUserPassword feature added

---
+++ OnionAppTraining.Infrastructure/Commands/User/ChangeUserPassowrd.cs
@@ -0,0 +1,8 @@
+﻿namespace OnionAppTraining.Infrastructure.Commands.User
+{
+    public class ChangeUserPassowrd : ICommand
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
---
+++ OnionAppTraining.Infrastructure/Handlers/User/ChangeUserPasswordHandler.cs
@@ -0,0 +1,14 @@
+﻿using OnionAppTraining.Infrastructure.Commands;
+using OnionAppTraining.Infrastructure.Commands.User;
+using System.Threading.Tasks;
+
+namespace OnionAppTraining.Infrastructure.Handlers.User
+{
+    public class ChangeUserPasswordHandler : ICommandHandler<ChangeUserPassowrd>
+    {
+        public async Task HandleAsync(ChangeUserPassowrd command)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
