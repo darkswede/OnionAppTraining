efe0d0ac1dd8baf57f958fc123092ad78b97ad17
Marcin <m.pietrucha0@gmail.com>
2022-01-12 16:28:54 +0100

    adding routes to driver

---
+++ OnionAppTraining.Infrastructure/Services/DriverRouteService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using OnionAppTraining.Core.Domain;
+using OnionAppTraining.Core.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace OnionAppTraining.Infrastructure.Services
+{
+    public class DriverRouteService : IDriverRouteService
+    {
+        private readonly IDriverRepository _driverRepository;
+        private readonly IMapper _mapper;
+
+        public DriverRouteService(IDriverRepository driverRepository, IMapper mapper)
+        {
+            _driverRepository = driverRepository;
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(Guid userId, string name, double startLongitude, double startLatitude, double endLongitude, double endLatitude)
+        {
+            var driver = await _driverRepository.GetByIdAsync(userId);
+            if (driver == null)
+            {
+                throw new Exception("driver with given ID do not exist");
+            }
+            var start = Node.Create("Start Address", startLongitude, endLatitude);
+            var end = Node.Create("End Address", endLongitude, endLatitude);
+
+            driver.AddRoute(name, start, end);
+            await _driverRepository.UpdateAsync(driver);
+        }
+
+        public async Task RemoveAsync(Guid userId, string name)
+        {
+            var driver = await _driverRepository.GetByIdAsync(userId);
+            if (driver == null)
+            {
+                throw new Exception("driver with given ID do not exist");
+            }
+
+            driver.RemoveRoute(name);
+            await _driverRepository.UpdateAsync(driver);
+        }
+    }
+}
---
+++ OnionAppTraining.Infrastructure/Services/IDriverRouteService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OnionAppTraining.Infrastructure.Services
+{
+    public interface IDriverRouteService
+    {
+        Task AddAsync(Guid userId, string name, double startLongitude, double startLatitude, double endLongitude, double endLatitude);
+        Task RemoveAsync(Guid userId, string name);
+    }
+}
