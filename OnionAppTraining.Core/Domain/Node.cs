e9461a4e40de14c0058e0e8c9d578b725f28cc7a
Marcin <m.pietrucha0@gmail.com>
2022-01-12 16:03:07 +0100

    node and route domain class expanded

--- OnionAppTraining.Core/Domain/Node.cs
+++ OnionAppTraining.Core/Domain/Node.cs
@@ -7,5 +7,56 @@ namespace OnionAppTraining.Core.Domain
         public string Address { get; protected set; }
         public double Longitude { get; protected set; }
         public double Latitude { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+
+        protected Node(string address, double longitude, double latitude)
+        {
+            Address = address;
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+
+        public void SetAddress(string address)
+        {
+            if (string.IsNullOrWhiteSpace(address))
+            {
+                throw new Exception($"Address is empty");
+            }
+
+            Address = address;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void SetLongitude(double longitude)
+        {
+            if (double.IsNaN(longitude))
+            {
+                throw new Exception("Longitude must be a number");
+            }
+            if (Longitude == longitude)
+            {
+                return;
+            }
+
+            Longitude = longitude;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void SetLatitude(double latitude)
+        {
+            if (double.IsNaN(latitude))
+            {
+                throw new Exception("Latitude must be a number");
+            }
+            if (Latitude == latitude)
+            {
+                return;
+            }
+
+            Latitude = latitude;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public static Node Create(string address, double longitude, double latitude) => new Node(address, longitude, latitude);
     }
 }
--- OnionAppTraining.Core/Domain/Route.cs
+++ OnionAppTraining.Core/Domain/Route.cs
@@ -4,8 +4,31 @@ namespace OnionAppTraining.Core.Domain
 {
     public class Route
     {
-        public Guid Id { get; protected set; }
-        public Node StartNode { get; protected set; }
-        public Node EndNode { get; protected set; }
+        public string Name { get; protected set; }
+        public Node Start { get; protected set; }
+        public Node End{ get; protected set; }
+        public double Distance { get; protected set; }
+        public DateTime UpdatedAt { get; set; }
+
+        protected Route(string name, Node start, Node end, double distance)
+        {
+            Name = name;
+            Start = start;
+            End = end;
+            Distance = distance;
+        }
+
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new Exception("Name cannot be empty");
+            }
+
+            Name = name;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public static Route Create(string name, Node start, Node end, double distance) => new Route(name, start, end, distance);
     }
 }
