11a2b028780f4ae937cfedda858e618b321db3dc
Marcin <m.pietrucha0@gmail.com>
2022-01-17 15:24:10 +0100

    exceptionhandler middleware add

---
+++ OnionAppTraining.Api/Framework/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Builder;
+using OnionAppTraining.Api.Framework.Middleware;
+
+namespace OnionAppTraining.Api.Framework
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder UseExceptionHandler(this IApplicationBuilder builder) => builder.UseMiddleware(typeof(ExceptionHandlerMiddleware));
+    }
+}
---
+++ OnionAppTraining.Api/Framework/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OnionAppTraining.Api.Framework.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch(Exception exception)
+            {
+                await HandleExceptionAsync(httpContext, exception);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+        {
+            var errorCode = "error";
+            var statusCode = HttpStatusCode.BadRequest;
+            var exceptionType = exception.GetType();
+
+            switch (exception)
+            {
+                case Exception ex when exceptionType == typeof(UnauthorizedAccessException):
+                    statusCode = HttpStatusCode.Unauthorized;
+                    break;
+
+                default:
+                    statusCode = HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            var reponse = new { code = errorCode, message = exception.Message };
+            var payload = JsonConvert.SerializeObject(reponse);
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)statusCode;
+
+            return httpContext.Response.WriteAsync(payload);
+        }
+    }
+}
