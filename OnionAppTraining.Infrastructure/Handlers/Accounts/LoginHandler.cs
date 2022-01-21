4bc31478a1bc6c984b2b6208f3ecd34cb6e4f20d
Marcin <m.pietrucha0@gmail.com>
2022-01-21 18:36:08 +0100

    loginHandler added

---
+++ OnionAppTraining.Infrastructure/Handlers/Accounts/LoginHandler.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using OnionAppTraining.Infrastructure.Commands;
+using OnionAppTraining.Infrastructure.Commands.User;
+using OnionAppTraining.Infrastructure.Extensions;
+using OnionAppTraining.Infrastructure.Services;
+using System.Threading.Tasks;
+
+namespace OnionAppTraining.Infrastructure.Handlers.Accounts
+{
+    public class LoginHandler : ICommandHandler<Login>
+    {
+        private readonly IUserService _userservice;
+        private readonly IHandler _handler;
+        private readonly IJwtHandler _jwtHandler;
+        private readonly IMemoryCache _memorycache;
+
+        public LoginHandler(IUserService userService, IHandler handler, IJwtHandler jwtHandler, IMemoryCache memoryCache)
+        {
+            _userservice = userService;
+            _handler = handler;
+            _jwtHandler = jwtHandler;
+            _memorycache = memoryCache;
+        }
+
+        public async Task HandleAsync(Login command) => await _handler
+            .RunAsync(async () => await _userservice.LoginAsync(command.Email, command.Password))
+            .Next()
+            .RunAsync(async () =>
+            {
+                var user = await _userservice.GetByEmailAsync(command.Email);
+                var jwt = _jwtHandler.CreateToken(user.Id, user.Role);
+                _memorycache.SetJwt(command.TokentId, jwt);
+            })
+            .ExecuteAsync();
+
+
+        //public async Task HandleAsync(Login command)
+        //{
+        //    await _userservice.LoginAsync(command.Email, command.Password);
+        //    var user = await _userservice.GetByEmailAsync(command.Email);
+        //    var jwt = _jwtHandler.CreateToken(user.Id, user.Role);
+        //    _memorycache.SetJwt(command.TokentId, jwt);
+        //}
+    }
+}
